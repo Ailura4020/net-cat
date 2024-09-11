2ac3be9106339f5b4b57c0db84aff38046673d42
dprouet <damienprouet76@gmail.com>
2024-09-11 15:02:53 +0200

    FinishedProject

--- main.go
+++ main.go
@@ -8,18 +8,24 @@ import (
 
 func main() {
 	//On defini le port du server
-	if len(os.Args) != 2 {
-		fmt.Println("Wrong number of arguments, usage: go run . [Port number]")
+	if len(os.Args) < 2 {
+		fmt.Println("Wrong number of arguments, usage: go run . [Port number] [IP adress]")
 		return
 	}
 	port := os.Args[1]
+	ip := ""
+	if len(os.Args) == 2 {
+		ip = "localhost"
+	} else {
+		ip = os.Args[2]
+	}
 
 	//On défini le port et l'ip du server
 	server := server.Server{
-		IP: "localhost",
+		IP: ip,
 		PORT: port,
 	}
-	
+
 	//On lance le server
 	server.Run()
 }
