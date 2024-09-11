401f6edd862192e12b7016b0988617c2aa117c9c
dprouet <damienprouet76@gmail.com>
2024-09-11 09:39:13 +0200

    connectAnddisconnectLog

--- server/Broadcast.go
+++ server/Broadcast.go
@@ -9,10 +9,31 @@ import (
 // ?Fonction qui envoie le message à tout les utilisateurs
 func (server *Server) Broadcast(client Client, message string, messagetype int) {
 	if messagetype == 0 {
+			//Enregistrement des informations du message dans le tableau de logs
+			historic := Historic{
+				Time:    time.Now().Format("2006-01-02 15:04:05"),
+				Pseudo:  message,
+				Message: " has joined the chat.\n",
+			}
+		
+			server.mutex.Lock()
+			Log = append(Log, historic)
+			server.mutex.Unlock()
 		for _, name := range server.clients {
 			name.conn.Write([]byte("\033[32m" + time.Now().Format("2006-01-02 15:04:05") + "] " + message + " has joined the chat.\n" + "\033[0m"))
 		}
 	} else if messagetype == 1 {
+		//Enregistrement des informations du message dans le tableau de logs
+		historic := Historic{
+			Time:    time.Now().Format("2006-01-02 15:04:05"),
+			Pseudo:  message,
+			Message: " has left the chat.\n",
+		}
+
+		server.mutex.Lock()
+		Log = append(Log, historic)
+		server.mutex.Unlock()
+
 		for i, name := range server.clients {
 			name.conn.Write([]byte("\033[31m" + "[" + time.Now().Format("2006-01-02 15:04:05") + "] " + message + " has left the chat.\n" + "\033[0m"))
 			if name == client {
@@ -35,7 +56,7 @@ func (server *Server) Broadcast(client Client, message string, messagetype int) 
 		if strings.HasPrefix(message, "/rename") {
 			newname := strings.Split(message, " ")
 			for i, name := range server.clients {
-				name.conn.Write([]byte(string(client.Pseudo) + " has changed his name for: " + newname[1]))
+				name.conn.Write([]byte(string(client.Pseudo) + " has changed his/her name for: " + newname[1]))
 				if name == client {
 					// fmt.Println("Before changes: ", server.clients[i].Pseudo)
 					// server.clients[i].Pseudo = newname[1][:len(newname[1])-1]
